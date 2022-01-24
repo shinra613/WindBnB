6cb41cd9f7fb1b47d19edf0a6e9af146f03bbac5
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2022-01-24 06:15:42 +0530

    style update
    -changed page content layout
    -worked on the responsiveness

--- appone/src/cards.js
+++ appone/src/cards.js
@@ -1,11 +1,23 @@
 import StarIcon from '@material-ui/icons/Star';
 
 const CardGenerator = (props) => {
+ let beds;
+   
+        if (props.beds != null) {
+        
+            beds = (<span> . {props.beds} beds</span>);
+            
+        } else { 
+            beds = (<span></span>);
+        }
+  
+
+
     return (
         <div className="card">
             <img className="card-image" src={props.image}></img>
             <div className="card-header">
-                <span className='CardGenSpanOne'>{props.type}.{props.beds} beds</span>
+                <span className='CardGenSpanOne'>{props.type}{beds}</span>
                 <div className='CardGenSpanTwo'><span><StarIcon  sx={{ color: "pink" }} /></span> <p>{props.rating}</p></div>
                 
             </div>
